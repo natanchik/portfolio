b06b75baf49270b14def8ed7d961d27dd54aa281
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-12-02 18:31:20 +0600

    feat: change styles and description

--- src/components/about-me.ts
+++ src/components/about-me.ts
@@ -1,8 +1,8 @@
 export const aboutMe = document.createElement('div');
 aboutMe.classList.add('about-me');
 
-const aboutMeText = `<p>I have been interested in programming all my life.\n
-I started my path in IT with Python, took a course in Deep Learning, and then got acquainted with web development.\n
-And I realized that I had found my direction.\n
-Now my main goal is to become a professional <strong>Frontend developer</strong>.</p>`;
+const aboutMeText = `<p>I have been interested in programming all my life.
+I started my path in IT with Python, got to know Deep Learning, and then got into web development.
+And I realized that I had found my direction.
+Now my main goal is to become a professional Frontend developer.</p>`;
 aboutMe.innerHTML = `<h2>About me</h2>` + aboutMeText;
